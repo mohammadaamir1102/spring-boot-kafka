0119180689f7607238899e40732dab3c1e4ab001
mohammadaamir1102 <86870356+mohammadaamir1102@users.noreply.github.com>
2023-06-22 19:03:00 +0530

    spring kafka project setup

---
+++ src/test/java/com/aamir/kafka/SpringBootApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aamir.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
